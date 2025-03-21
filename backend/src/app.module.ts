ed4b1d3b49d5b76d429c8dc3c47b4c82e8d6ccab
salamander53 <dotruongkhoa04@gmail.com>
2025-03-21 23:24:14 +0700

    khoa

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-    imports: [CatsModule],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [CatsModule, DatabaseModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
