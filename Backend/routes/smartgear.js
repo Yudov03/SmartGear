7d7bed9ac5078a1c01e010430a6628230215b55f
Duy <volydacduy942003@gmail.com>
2025-03-21 21:57:49 +0700

    duy update

--- Backend/routes/smartgear.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const smartgearcontroller = require('../controller/Controller');
-
-router.get('/', smartgearcontroller.getSmartgears);
-router.get('/:id', smartgearcontroller.viewSmartgear);
-module.exports = router;
