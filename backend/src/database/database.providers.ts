bd7766d5f119b84478167e52be22b96e2e4bd17d
salamander53 <dotruongkhoa04@gmail.com>
2025-03-25 17:53:14 +0700

    khoa

--- backend/src/database/database.providers.ts
+++ backend/src/database/database.providers.ts
@@ -4,31 +4,31 @@ import { Logger } from '@nestjs/common';
 const logger = new Logger('DatabaseConnection');
 
 export const databaseProviders = [
-  //   {
-  //     provide: 'DATA_SOURCE',
-  //     useFactory: async () => {
-  //       const dataSource = new DataSource({
-  //         type: 'mssql',
-  //         host: 'khoa.database.windows.net',
-  //         port: 1433,
-  //         username: 'khoa123',
-  //         password: 'khoa@123',
-  //         database: 'Cinema',
-  //         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  //         synchronize: false,
-  //         options: {
-  //           encrypt: true,
-  //           trustServerCertificate: true,
-  //         },
-  //       });
-  //       try {
-  //         await dataSource.initialize();
-  //         logger.log('Database connection established successfully');
-  //         return dataSource;
-  //       } catch (error) {
-  //         logger.error('Error during database connection:', error);
-  //         throw error;
-  //       }
-  //     },
-  //   },
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'sqlite',
+        // host: 'khoa.database.windows.net',
+        // port: 1433,
+        // username: 'khoa123',
+        // password: 'khoa@123',
+        database: 'database.sqlite',
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+        // options: {
+        //   encrypt: true,
+        //   trustServerCertificate: true,
+        // },
+      });
+      try {
+        await dataSource.initialize();
+        logger.log('Database connection established successfully');
+        return dataSource;
+      } catch (error) {
+        logger.error('Error during database connection:', error);
+        throw error;
+      }
+    },
+  },
 ];
