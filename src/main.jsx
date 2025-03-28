4087e216fb45bbb76b422e0c66866fbe72e3e3d9
salamander53 <dotruongkhoa04@gmail.com>
2025-03-28 18:40:49 +0700

    khoa

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,23 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import '/src/index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import WelcomePage from './pages/WelcomePage.jsx';
-import CatalogPage from './pages/CatalogPage.jsx';
-import SettingPage from './pages/SettingPage.jsx';
-import BuilderPage from './pages/BuilderPage.jsx';
-import Homepage from './pages/Homepage.jsx';
-import ProfilePage from './pages/ProfilePage.jsx';
-import HistoryPage from './pages/HistoryPage.jsx';
-import RootLayout from './components/RootLayout.jsx';
-import { Login } from './Login/Login.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "/src/index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+import WelcomePage from "./pages/WelcomePage.jsx";
+import CatalogPage from "./pages/CatalogPage.jsx";
+import SettingPage from "./pages/SettingPage.jsx";
+import BuilderPage from "./pages/BuilderPage.jsx";
+import Homepage from "./pages/Homepage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
+import HistoryPage from "./pages/HistoryPage.jsx";
+import RootLayout from "./components/RootLayout.jsx";
+import { Login } from "./login/login.jsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <RootLayout />,
     children: [
       // Route mặc định: khi truy cập '/', hiển thị WelcomePage
@@ -24,39 +27,38 @@ const router = createBrowserRouter([
       },
       // Các route ứng dụng khi người dùng đã đăng nhập thành công
       {
-        path: 'home',
+        path: "home",
         element: <Homepage />,
       },
       {
-        path: 'builder',
+        path: "builder",
         element: <BuilderPage />,
       },
       {
-        path: 'catalog',
+        path: "catalog",
         element: <CatalogPage />,
       },
       {
-        path: 'setting',
+        path: "setting",
         element: <SettingPage />,
       },
       {
-        path: 'profile',
+        path: "profile",
         element: <ProfilePage />,
       },
       {
-        path: 'history',
+        path: "history",
         element: <HistoryPage />,
       },
     ],
   },
   {
-    path: '/login',
+    path: "/login",
     element: <Login />,
   },
-
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>
