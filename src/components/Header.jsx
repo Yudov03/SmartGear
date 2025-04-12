75cfee90f337e675e3be9dba63cb20e9dcd0cf94
GhostDevil274 <minhanhnguyentran274@gmail.com>
2025-04-12 13:14:51 +0700

    Add navigation to ProfilePage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Header = () => {
   const location = useLocation().pathname.substring(1).toUpperCase();
+  const navigate = useNavigate();
 
   return (
     <div className="w-full bg-[#4FD1C5] py-4 ">
@@ -12,29 +13,32 @@ const Header = () => {
           <p className="text-white font-bold text-md">{location}</p>
         </div>
 
-        <div className="d-flex gap-4 me-4">
-          <div className=" flex items-center w-[200px] h-[40px] p-2 border border-[#E2E8F0] rounded-[15px] bg-white shadow-sm">
-              <input
-                type="text"
-                placeholder="Type here..."
-                className="ml-[10px] flex-1 outline-none border-none bg-transparent text-gray-700 px-3"
-              />
-            </div>
+        <div className="flex gap-4">
+          <div className="flex items-center w-[200px] h-[40px] p-2 border border-[#E2E8F0] rounded-[15px] bg-white shadow-sm">
+            <input
+              type="text"
+              placeholder="Type here..."
+              className="ml-[10px] flex-1 outline-none border-none bg-transparent text-gray-700 px-3"
+            />
+          </div>
 
-          <div className="flex items-center ml-[10px] gap-2 ">
-              <button className="btn">
+          <div
+            className="flex items-center ml-[10px] gap-2 cursor-pointer"
+            onClick={() => navigate("/profile")}
+          >
+            <button className="btn">
                 <img
                   src="../public/Profile.svg"
                   className="w-[20px] h-[20px]"
                   alt="User"
                 />
               </button>
-              <span className=" text-gray-700">Duy Vo</span>
+            <span className="text-gray-700">Duy Vo</span>
           </div>
 
           <button className="btn mt-[4px]">
             <img
-              src="../public/Notification.svg"
+              src="/Notification.svg"
               className="w-[25px]"
               alt="Notification"
             />
